c094b349f8319944f7502a4daf4361e55c718191
Rachel Brodsky <rachelbrodsky@Rachels-MacBook-Pro.local>
2021-10-07 23:56:08 -0400

    working module 8 assignment

--- module8_assignment/app.js
+++ module8_assignment/app.js
@@ -19,7 +19,7 @@ function FoundItemsDirective() {
     },
     controller: NarrowItDownController,
     controllerAs: 'list',
-    bindToController: true
+    bindToController: true,
   };
 
   return ddo;
@@ -30,22 +30,31 @@ NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var list = this;
 
-  list.logFoundItems = function (searchTerm) {
-    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-    
-    promise.then(function (response) {
-      list.items = response;
-      console.log(list.items);
-    })
-    .catch(function (error) {
-      console.log("Error occurred in NarrowItDownController.");
-    })
+  list.addItems = function(searchTerm) {
+    if ((searchTerm === undefined) || (!searchTerm)) {
+      list.error = true;
+    }
+
+    else {
+      list.error = false;
+      var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+      
+      promise.then(function (response) {
+        list.items = response;
+        if (list.items.length === 0) {
+          list.error = true;
+        }
+      })
+      .catch(function (error) {
+        console.log("Error occurred in NarrowItDownController.");
+      })
+
+    }
   };
 
   list.removeItem = function (itemIndex) {
     MenuSearchService.removeItem(itemIndex);
   };
-
 }
 
 
@@ -55,33 +64,36 @@ function MenuSearchService($http, ApiPath) {
   var foundItems = [];
 
   service.getMatchedMenuItems = function (searchTerm) {
-    return $http({
-      method: "GET",
-      url: (ApiPath)
-    }).then(function (response) {
-      var menuItems = response.data.menu_items;
-      Object.keys(menuItems).forEach(function(key) {
-        var item = menuItems[key];
-        var itemName = item['name'];
-        if (itemName.toLowerCase().indexOf(searchTerm) !== -1) {
-          foundItems.push(item);
-        }
+    if ((searchTerm === undefined) || (!searchTerm)) {
+      // reset items to empty list
+      foundItems = [];
+    }
+    else {
+      foundItems = [];
+      return $http({
+        method: "GET",
+        url: (ApiPath)
+      }).then(function (response) {
+        var menuItems = response.data.menu_items;
+        Object.keys(menuItems).forEach(function(key) {
+          var item = menuItems[key];
+          var description = item['description'];
+          if (description.toLowerCase().indexOf(searchTerm) !== -1) {
+            foundItems.push(item);
+          }
+        });
+        return foundItems;
+      })
+      .catch(function() {
+        console.log("Error occurred in MenuSearchService");
       });
-      return foundItems;
-    })
-    .catch(function() {
-      console.log("Error occurred in MenuSearchService");
-    });
+    }
   };
+  
 
   service.removeItem = function (itemIndex) {
     foundItems.splice(itemIndex, 1);
   };
-
-  service.getItems = function () {
-    return foundItems;
-  };
-
 }
 
 })();
